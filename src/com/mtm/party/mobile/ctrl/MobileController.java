0747afdc93e88e9cc4ff9791b51f6b19af6deb74
forlangel <avalon@forlangel.com>
2018-09-20 14:20:40 +0800

    complete

--- src/com/mtm/party/mobile/ctrl/MobileController.java
+++ src/com/mtm/party/mobile/ctrl/MobileController.java
@@ -185,8 +185,7 @@ public class MobileController {
 	 */
 	private String savePraise(HttpServletRequest request,
 			HttpServletResponse response) throws UnsupportedEncodingException {
-		String nickName = request.getParameter("nickName");
-		//String nickName = new String(request.getParameter("nickName").getBytes("ISO-8859-1"),"UTF-8");
+		String nickName = new String(request.getParameter("nickName").getBytes("ISO-8859-1"),"UTF-8");
 		String nickImage = request.getParameter("nickImage");
 		String openId = request.getParameter("openId");
 		try {
@@ -223,10 +222,8 @@ public class MobileController {
 	private String saveComment(HttpServletRequest request,
 			HttpServletResponse response)throws Exception {
 		
-		//String nickName = new String(request.getParameter("nickName").getBytes("ISO-8859-1"),"UTF-8");
-		//String comment = new String(request.getParameter("comment").getBytes("ISO-8859-1"),"UTF-8");
-		String nickName = request.getParameter("nickName");
-		String comment = request.getParameter("comment");
+		String nickName = new String(request.getParameter("nickName").getBytes("ISO-8859-1"),"UTF-8");
+		String comment = new String(request.getParameter("comment").getBytes("ISO-8859-1"),"UTF-8");
 		String nickImage = request.getParameter("nickImage");
 		String time = request.getParameter("time");
 		String openId = request.getParameter("openId");
